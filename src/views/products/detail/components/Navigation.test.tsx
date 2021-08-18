ba28414dc4ec8fd2a4eb3c3af8c294a9b69e1b38
dirac <767848898@qq.com>
2021-08-18 09:49:45 +0800

    navigation 添加测试

--- src/views/products/detail/components/Navigation.test.tsx
+++ src/views/products/detail/components/Navigation.test.tsx
@@ -1,26 +1,64 @@
-import { render as rtlRender } from "@testing-library/react"
-import React, { FC } from "react"
-import Navigation from "./Navigation"
+import { fireEvent, render as rtlRender } from "@testing-library/react";
+import React, { FC } from "react";
+import Navigation from "./Navigation";
 
 const render = (ui: React.ReactElement) => {
   const Wrapper: FC = (props) => {
-    return <div style={{height: '500vh'}}>
-      {props.children}
-    </div>
-  }
-  return rtlRender(ui, {wrapper: Wrapper})
-}
+    return <div style={{ height: "500vh" }}>{props.children}</div>;
+  };
+  return rtlRender(ui, { wrapper: Wrapper });
+};
 
-describe('Navigation', () => {
-  it('initial render component', () => {
-    const navList = [{name: 'test1', id: '1'}, {name: 'test2', id: '2'}]
-    const {queryAllByText, getAllByRole, getByTestId} = render(<Navigation navList={navList} />)
+describe("Navigation", () => {
+  it("initial render component", () => {
+    const navList = [
+      { name: "test1", id: "1" },
+      { name: "test2", id: "2" },
+    ];
+    const { queryAllByText, getAllByRole, getByTestId } = render(
+      <Navigation navList={navList} />
+    );
 
-    expect(queryAllByText(/test/).length).toBe(navList.length)
-    expect(getByTestId('header')).toHaveStyle({backgroundColor: 'rgba(255,255,255,0)'})
+    expect(queryAllByText(/test/).length).toBe(navList.length);
+    expect(getByTestId("header")).toHaveStyle({
+      backgroundColor: "rgba(255,255,255,0)",
+    });
     getAllByRole(/icon/).forEach((icon) => {
-      expect(icon).toHaveStyle({color: 'white'})
-    })
+      expect(icon).toHaveStyle({ color: "white" });
+    });
+  });
 
-  })
-})
+  it("should change styles with scroll distance", () => {
+    const threshold = 100 
+    const { getAllByRole, getByTestId } = render(
+      <Navigation navList={[]} threshold={threshold}/>
+    );
+
+    fireEvent.scroll(document.body, {target: {scrollY: threshold / 2}})
+
+    expect(getByTestId("header")).toHaveStyle({
+      backgroundColor: "rgba(255,255,255,0.5)",
+    });
+    getAllByRole(/icon/).forEach((icon) => {
+      expect(icon).toHaveStyle({ color: "white" });
+    });
+
+    fireEvent.scroll(document.body, {target: {scrollY: threshold}})
+
+    expect(getByTestId("header")).toHaveStyle({
+      backgroundColor: "rgba(255,255,255,1)",
+    });
+    getAllByRole(/icon/).forEach((icon) => {
+      expect(icon).toHaveStyle({ color: "black" });
+    });
+
+    fireEvent.scroll(document.body, {target: {scrollY: threshold * 2}})
+
+    expect(getByTestId("header")).toHaveStyle({
+      backgroundColor: "rgba(255,255,255,1)",
+    });
+    getAllByRole(/icon/).forEach((icon) => {
+      expect(icon).toHaveStyle({ color: "black" });
+    });
+  });
+});
